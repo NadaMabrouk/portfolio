e64f4124c2b46f55884eba8a5db5caeec464f2c4
Nada Mabrouk <nada.mohamed.1991@gmail.com>
2021-07-06 11:15:43 -0700

    Sidebar created using Nav and Navbar React components

---
+++ src/components/Sidebar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import {Nav, Navbar} from 'react-bootstrap'
+
+class Sidebar extends Component{
+    state = {
+        collapse: false,
+    }
+    toggle = (expanded) => {
+        this.setState( {
+            collapse: expanded
+        })
+    }
+    closeNav = () => {
+        this.setState({
+            collapse: false
+        })
+    }
+    render(){
+       
+        return(
+               <Navbar className="navbar" defaultactivekey="/home" expand="lg" onToggle={this.toggle} expanded={this.state.collapse} >
+                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                 <Navbar.Collapse  id="basic-navbar-nav" >
+                    <Nav className="mr-auto flex-column sidebar" onClick={this.closeNav}>
+                        <Navbar.Brand href="#home">
+                            <img
+                                src="https://i.postimg.cc/8C4kjkSs/personal-Photo.jpg"
+                                width="150"
+                                height="150"
+                                alt="Nada Mabrouk"
+                            />
+                        </Navbar.Brand>
+                        <h4>Nada Mabrouk</h4>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link eventKey="#about">About</Nav.Link>
+                        <Nav.Link eventKey="#skills">Skills</Nav.Link>
+                        <Nav.Link eventKey="#projects">Projects</Nav.Link>
+                        <Nav.Link eventKey="#contact">Contact</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+        )
+    }
+}
+
+export default Sidebar;
+
