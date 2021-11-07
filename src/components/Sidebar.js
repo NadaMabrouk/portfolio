7f1cd324ad11572786b7e2f4fc136fb4a4768758
Nada Mabrouk <nada.mohamed.1991@gmail.com>
2021-11-07 11:59:38 -0800

    Changed gow sidebar works

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import {Nav, Navbar} from 'react-bootstrap'
+import Scrollspy from 'react-scrollspy'
 import '../styles/sidebar.css'
 
 class Sidebar extends Component{
     state = {
         collapse: false,
-        activeKey: "#home"
     }
 
     toggle = (expanded) => {
@@ -18,16 +18,14 @@ class Sidebar extends Component{
             collapse: false
         })
     }
-    handleSelect = (selectedKey) => {
-        this.setState({activeKey: selectedKey});
-    }
+ 
     render(){  
         
         return(
                <Navbar className="navbar" expand="md" onToggle={this.toggle} expanded={this.state.collapse} >
                  <Navbar.Toggle aria-controls="basic-navbar-nav" className={this.state.collapse?"transformBtn":""}/>
                  <Navbar.Collapse  id="basic-navbar-nav" >
-                    <Nav className="mr-auto flex-column sidebar" id="side-bar" activeKey={this.state.activeKey} onSelect={this.handleSelect} onClick={this.closeNav}>
+                    <Nav className="mr-auto flex-column sidebar" id="side-bar" onClick={this.closeNav}>
                         <Navbar.Brand href="#home">
                             <img
                                 src="https://i.postimg.cc/8C4kjkSs/personal-Photo.jpg"
@@ -37,11 +35,13 @@ class Sidebar extends Component{
                             />
                         </Navbar.Brand>
                         <h4>Nada Mabrouk</h4>
-                        <Nav.Link href="#home" >Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
+                        <Scrollspy items={ ['home', 'about', 'skills','projects','contact']} offset={-40} currentClassName="active">
+                            <a href="#home" className="nav-link">Home</a>
+                            <a href="#about" className="nav-link">About</a>
+                            <a href="#skills" className="nav-link">Skills</a>
+                            <a href="#projects" className="nav-link">Projects</a>
+                            <a href="#contact" className="nav-link">Contact</a>
+                        </Scrollspy>
                     </Nav>
                     </Navbar.Collapse>
                 </Navbar>
